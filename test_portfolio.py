1e578b4ca6379f81ac809feb73b06435bef436a6
guanglinghelen <93166776+guanglinghelen@users.noreply.github.com>
2021-11-15 21:03:04 -0500

    Update test_portfolio.py

--- test_portfolio.py
+++ test_portfolio.py
@@ -1,5 +1,9 @@
 from portfolio import Portfolio
 
+def test_empty_portfolio():
+    p=Portfolio()
+    assert p.cost()== 0.0
+
 def test_buy_one_stock():
     p = Portfolio()
     p.buy("IBM", 100, 176.48)
