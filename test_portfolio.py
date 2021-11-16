3df0498af28896b0336b4bca78674510f70d93b8
guanglinghelen <93166776+guanglinghelen@users.noreply.github.com>
2021-11-15 21:03:33 -0500

    Update test_portfolio.py

--- test_portfolio.py
+++ test_portfolio.py
@@ -2,7 +2,7 @@ from portfolio import Portfolio
 
 def test_empty_portfolio():
     p=Portfolio()
-    assert p.cost()== 0.0
+    assert p.cost()== 10000.0
 
 def test_buy_one_stock():
     p = Portfolio()
